6bfd9d4d2cedec2873d0a58a5d9e66c2f51aa7e1
vikash <vikashkelly@gmail.com>
2026-02-12 08:34:45 +0530

    Add sum of numbers and while loop examples

---
+++ loop/sumofnumbers.py
@@ -0,0 +1,6 @@
+m = int(input("enter the 1st number: "))
+n = int(input("enter the 2nd number: "))
+sum = 0
+for x in range(m,n+1):
+    sum +=x
+print(sum)
---
+++ loop/while.py
@@ -0,0 +1,5 @@
+j = int(input("enter the number: "))
+i=1
+while i<=j:
+    print("*",end="")
+    i +=1
