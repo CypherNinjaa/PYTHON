ecfb0429e0d592cdd93ef87bfbf280c35031612c
vikash <vikashkelly@gmail.com>
2026-02-12 09:54:45 +0530

    Add sum of series calculations for 1/n and 1/n^2

--- assessments/sum of series2.py
+++ assessments/sum of series2.py
@@ -1,6 +1,6 @@
-# sum of series: (1+1/2.....+1/n)
+# sum of series: (1+1/2**2.....+1/n**2)
 num = int(input("Enter the number: "))
 sum =0
 for i in range(1,num+1):
-    sum = sum+1/i
+    sum = sum+1/(i**2)
 print("sum of series: ",sum)
