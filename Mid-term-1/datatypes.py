df2206852c2d6a3f6fd8bdc5fd59b458723193ab
vikash <vikashkelly@gmail.com>
2026-03-27 17:03:22 +0530

    Add keyword module usage and data types examples

---
+++ Mid-term-1/datatypes.py
@@ -0,0 +1,27 @@
+# numeric 
+a = 10;
+b = 4.4
+c = 4+4j
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# sequence type
+# string
+s = "welcoem to the geeks world"
+
+print (type(s))
+print(s[0])
+print(s[-1])
+
+# list
+a = [1,2,3,4]
+print(a)
+print(a[-1])
+# tuples
+tup1 = (1, 2, 3, 4, 5)
+# access tuple items
+print(tup1[0])
+print(tup1[-1])
+print(tup1[-3])
