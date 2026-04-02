ac18dad5c033ec60d2ca2cd0ad0692872536f3db
vikash <vikashkelly@gmail.com>
2026-04-02 13:25:43 +0530

    Add even/odd checker and factorial calculator classes; include SVG download icon

---
+++ classobj/evenodd_factorial.py
@@ -0,0 +1,21 @@
+# Parent class
+class evenodd:
+    def Evenodd(self, a):
+        if a % 2 == 0:
+            print("It is an even number")
+        else:
+            print("It is an odd number")
+
+# Child class
+class fact(evenodd):
+    def factorial(self, n):
+        fact = 1
+        for i in range(1, n + 1):
+            fact *= i
+        print("Factorial is:", fact)
+
+
+obj = fact()
+
+obj.Evenodd(5)
+obj.factorial(5)
